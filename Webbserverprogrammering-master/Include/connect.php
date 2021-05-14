63174b7e715694b040aca140e35d3e0c4f380111
02Patrick02 <54435353+02Patrick02@users.noreply.github.com>
2021-05-14 19:52:35 +0200

    Add files via upload

--- Webbserverprogrammering-master/Include/connect.php
+++ Webbserverprogrammering-master/Include/connect.php
@@ -1,5 +1,5 @@
 <?php //kopplar till en databas
-	$dbh = new mysqli("localhost", "patrick", "password", "blogg");
+	$dbh = new mysqli("localhost", "username", "password", "blogg");
 	
 	if(!$dbh) {
 		echo "Ingen kontakt med databasen";
